a22114124bb377ed6610a870b0d76862922d080f
keszotrab <39887624+keszotrab@users.noreply.github.com>
2022-05-20 11:30:09 +0200

    Add files via upload

---
+++ lab_8_prOb/lab_8_prOb/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading;
+
+
+
+
+
+
+
+
+/*
+	public static string Whatever_string(string a, string b)
+	{
+		string bambam = a + " " + b;
+		return bambam;
+		
+	}
+*/
+
+
+class bass 
+{ 
+
+
+
+
+}
+//public delegate string parada(string a, string b);
+
+public delegate void Del(string message);
+
+
+
+public class Program
+	{
+		public static void Main(string[] args)
+		{
+			string x = "cykcyk";
+			string y = "bambam";
+
+
+
+			Thread thread = new Thread(() =>
+			{
+				for (int i = 1; i <= 5; ++i)
+				{
+					Console.WriteLine("Iteration: " + i);
+					Thread.Sleep(1000); // sleeps created thread for 1 second
+			}
+			});
+
+			thread.Start();
+
+
+
+
+			Thread babumts = new Thread(() =>
+			{
+				Console.WriteLine("asd");
+			});
+			babumts.Start();
+
+
+			Thread skraaa = new Thread(() =>
+			{
+				Console.WriteLine("sadasd");
+			});
+			skraaa.Start();
+
+			
+		
+		
+		}
+
+		
+
+
+
+
+
+	}
